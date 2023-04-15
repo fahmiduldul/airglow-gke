2329bd0137b536df7066a53989e3688c2f18aea2
fahmiduldul <afahmi13@live.com>
2023-04-15 12:45:17 +0700

    feat: subnetwork

---
+++ terraform/3-subnets.tf
@@ -0,0 +1,18 @@
+resource "google_compute_subnetwork" "private" {
+  name          = "private"
+  ip_cidr_range = "10.0.0.0/16"
+  network       = google_compute_network.airflow_vpc.id
+
+  private_ip_google_access = true
+
+  secondary_ip_range = [
+    {
+      range_name = "k8s-pod-range",
+      ip_cidr_range = "10.1.0.0/16"
+    },
+    {
+      range_name = "k8s-service-range",
+      ip_cidr_range = "10.2.0.0/16"
+    }
+  ]
+}
